904607b1d660128e295fe584d5369f5a37f4059f
aneesmt2006 <aneesanu2006@gmail.com>
2025-09-14 11:38:40 +0530

    gRPC-comit-1

---
+++ server.js
@@ -0,0 +1,43 @@
+const grpc = require("@grpc/grpc-js");
+const PROTO_PATH = "./news.proto";
+console.log(PROTO_PATH)
+var protoLoader = require("@grpc/proto-loader");
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const newsProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+let news = [
+  { id: "1", title: "Note 1", body: "Content 1", postImage: "Post image 1" },
+  { id: "2", title: "Note 2", body: "Content 2", postImage: "Post image 2" },
+
+  ]  ;
+
+server.addService(newsProto.NewsService.service, {
+  
+  getAllNews: (_, callback) => {
+    console.log('news from server reached',news)
+   return callback(null, {news});
+  },
+});
+
+server.bindAsync(
+  "127.0.0.1:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+
+     if (error) {
+      console.error(err);
+      return;
+    }
+    console.log("Server running at http://127.0.0.1:50051");
+    
+  }
+);
